5f5811f866f18ead6b26f74aecc6c9df5954c312
Roman <r0m4uk0106@gmail.com>
2023-02-22 22:41:49 +0300

    Transferred the project to webpack

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PostcssPresetEnv = require("postcss-preset-env");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+  },
+  entry: "./src/index.tsx",
+  output: {
+    assetModuleFilename: "assets/[contenthash:8].[ext]",
+    clean: true,
+    filename: "[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [PostcssPresetEnv],
+              }
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|webp|gif)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[ext]"
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css"
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+}
