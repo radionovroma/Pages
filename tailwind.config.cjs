38148f2b5a8884048f5cd249ebbbbeda9578f840
Roman <r0m4uk0106@gmail.com>
2023-03-04 18:38:17 +0300

    Changed the parameters of the tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,17 +4,37 @@ module.exports = {
     './public/index.html'
   ],
   theme: {
-    extends: {
-      colors: {
-        blue: '#1b3764',
-        yellow: '#ffca42',
-        gray: '#f6f8fc',
-      },
-      fontFamily: {
-        serif: ['Cardo', 'serif'],
-        sans: ['Inter', 'sans-serif'],
-      },
+    colors: {
+      blue: '#1b3764',
+      lightBlue: '#b4c7e7',
+      yellow: '#ffca42',
+      gray: '#f6f8fc',
+      white: '#ffffff',
     },
+    fontFamily: {
+      serif: ['Cardo', 'serif'],
+      sans: ['Inter', 'sans-serif'],
+    },
+    rotate: {
+      '45': '45deg',
+      '315': '315deg',
+    },
+    spacing: {
+      '0': '0px',
+      '5': '5px',
+      '10': '10px',
+      '20': '20px',
+      '25': '25px',
+      '30': '30px',
+      '40': '40px',
+      '65': '65px',
+      '110': '110px',
+      '230': '230px',
+      '350': '350px',
+      '660': '660px',
+      'cont': '1300px',
+      'full': '100%',
+    }
   },
   plugins: [],
 }
