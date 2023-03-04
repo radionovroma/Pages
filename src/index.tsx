64da43a1b9e11942939c8ab8bd996547d49c6da9
Roman <r0m4uk0106@gmail.com>
2023-03-04 19:14:54 +0300

    Connected the App to the storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "./App";
 import { makeServer } from "@mock";
+import { store } from "@store/store"
 import "./styles/index.scss";
 
 makeServer();
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App/>);
+  .render(
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  );
