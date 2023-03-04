d5ce22e2bf64ee1447aa044f8bb368d7d1472f96
Roman <r0m4uk0106@gmail.com>
2023-03-04 18:36:04 +0300

    Deleted unused dependencies, added @svgr/webpack and classnames to dependencies, configured aliases by TsconfigPathsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
 const PostcssPresetEnv = require('postcss-preset-env')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
@@ -9,106 +10,99 @@ const devMode = mode === 'development'
 const target = devMode ? 'web' : 'browserslist'
 const devtool = devMode ? 'source-map' : undefined
 
-module.exports = {
-  mode,
-  target,
-  devtool,
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    open: true,
-  },
-  entry: './src/index.tsx',
-  output: {
-    assetModuleFilename: 'assets/[contenthash:8].[ext]',
-    clean: true,
-    filename: '[name].[contenthash:8].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: 'defaults' }]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.(c|sa|sc)ss$/i,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [PostcssPresetEnv],
-              }
+module.exports = () =>{
+  return {
+    mode,
+    target,
+    devtool,
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+    },
+    entry: './src/index.tsx',
+    output: {
+      assetModuleFilename: 'assets/[contenthash:8].[ext]',
+      clean: true,
+      filename: '[name].[contenthash:8].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(c|sa|sc)ss$/i,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [PostcssPresetEnv],
+                }
+              },
             },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|webp|gif)$/i,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|webp|gif)$/i,
+          use: [
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75
+                }
               }
             }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name].[ext]'
           }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader',
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name].[ext]'
-        }
-      },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash:8].css'
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:8].css'
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', 'jsx', '.js'],
+      plugins: [new TsconfigPathsPlugin({
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+      })],
+    },
+  }
 }
