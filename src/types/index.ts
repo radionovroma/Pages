d199c949e10173209ee324d3e68abfa734407d66
Roman <r0m4uk0106@gmail.com>
2023-03-04 18:54:54 +0300

    Added NavRoute and LOADED_STATUSES to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
-export * from "./category";
-export * from "./book";
+export type { Category } from "./category";
+export type { Book } from "./book";
+export * from "./loadStatuses";
+export type { NavRoute } from "./navRoute";
---
+++ src/types/navRoute.ts
@@ -0,0 +1,5 @@
+export interface NavRoute {
+  label: string;
+  id: string;
+  route: string;
+}
