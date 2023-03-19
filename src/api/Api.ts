d434c26e4208155f5c3fca18f770310dc93d66c9
Roman <r0m4uk0106@gmail.com>
2023-03-19 22:08:40 +0300

    Edited data query methods

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,15 +1,14 @@
-import { Category, Book } from "@types";
+import { Category, Book, SearchParams, FilterParams } from "@types";
 
 const CATEGORIES_URL = new URL('http://localhost:8080/api/categories');
 const BANNER_PRODUCTS_URL = new URL('http://localhost:8080/api/banner_products');
 const POPULAR_CATEGORIES_URL = new URL('http://localhost:8080/api/popular_categories');
+const PRODUCTS_URL = new URL('http://localhost:8080/api/products');
 const SUBSCRIBE_URL = new URL('http://localhost:8080/api/subscribe');
 
-type Options = { method: string, body: string }
-
 export class Api {
-  fetchData = (url: URL, options?: Options) => {
-    return fetch(url, options)
+  get = (url: URL): Promise<any> => {
+    return fetch(url)
       .then(data => {
         if (data.ok) {
           return data.json();
@@ -17,24 +16,45 @@ export class Api {
           throw new Error(`${data.statusText}`);
         }
       });
-  };
+
+  }
 
   getCategoriesList = (): Promise<Category[]> => {
-    return this.fetchData(CATEGORIES_URL);
+    return this.get(CATEGORIES_URL);
   };
 
   getBannerProducts = (): Promise<Book[]> => {
-    return this.fetchData(BANNER_PRODUCTS_URL)
+    return this.get(BANNER_PRODUCTS_URL)
   }
 
   getPopularCategories = (): Promise<{ category: Category, books: Book[] }[]> => {
-    return this.fetchData(POPULAR_CATEGORIES_URL);
+    return this.get(POPULAR_CATEGORIES_URL);
   }
 
-  subscribeNewsletter = (email: string) => {
-    return this.fetchData(SUBSCRIBE_URL, {
+  getProducts = (params ?: SearchParams): Promise<{ items: Book[], filterParams: FilterParams, total: number }> => {
+    const searchParams = new URLSearchParams({
+      ...params,
+    } as Record<string, string>).toString();
+    const url = new URL(PRODUCTS_URL);
+    url.search = searchParams;
+    return this.get(url);
+  };
+
+  post = (url: URL, body: Record<string, unknown>): Promise<any> => {
+    return fetch(url, {
       method: "POST",
-      body: JSON.stringify({ email }),
+      body: JSON.stringify(body),
     })
+      .then(data => {
+        if (data.ok) {
+          return data.json();
+        } else {
+          throw new Error(`${data.statusText}`);
+        }
+      })
+  };
+
+  postSubscriber = (body: Record<string, unknown>): Promise<{ subscribers: { email: string, id: string } }> => {
+    return this.post(SUBSCRIBE_URL, body);
   }
 }
