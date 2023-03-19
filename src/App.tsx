b39231fdad5f08c4551e26cf5419b5c529b3e739
Roman <r0m4uk0106@gmail.com>
2023-03-19 22:26:39 +0300

    Fixed the logic of getting routes for the Header and the Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategoriesList, getCategoriesList, getCategoriesStatus } from "@store/categories";
 import { MainPage } from "@containers";
+import { Footer, Header } from "@components";
+import { NavRoute } from "@types";
+import LogoSvg from "@img/logo.svg";
 
 export const App: FC = () => {
+  const categoriesList = useSelector(getCategoriesList);
+  const categoriesStatus = useSelector(getCategoriesStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategoriesList() as any);
+  }, []);
+
+  const routs: NavRoute[] = [
+    { label: "Home", id: "home", path: "./",  },
+    { label: "Catalog", id: "catalog", path: "./catalog", dropdownList: { list: categoriesList, status: categoriesStatus } },
+    { label: "Blog", id: "blog", path: "./blog" },
+    { label: "Contacts", id: "contacts", path: "./contacts" },
+  ];
+
   return (
-    <MainPage/>
+    <>
+      <Header
+        routs={routs}
+        LogoSvg={LogoSvg}/>
+      <MainPage/>
+      <Footer
+        routs={routs}
+        LogoSvg={LogoSvg}/>
+    </>
   )
 }
--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,42 +1,14 @@
 import { FC } from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCategoriesList, getCategoriesList, getCategoriesStatus } from "@store/categories";
-import { BannerSwiper, PopularCategories, Footer, Header, BannerBlock, NewsletterBlock } from "@components";
-import { NavRoute } from "@types";
-import LogoSvg from "@img/logo.svg";
+import { BannerSwiper, PopularCategories, BannerBlock, NewsletterBlock } from "@components";
 
 export const MainPage: FC = () => {
-  const categoriesList = useSelector(getCategoriesList);
-  const categoriesStatus = useSelector(getCategoriesStatus);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCategoriesList() as any);
-  }, []);
-
-  const routs: NavRoute[] = [
-    { label: "Home", id: "home", path: "./",  },
-    { label: "Catalog", id: "catalog", path: "./catalog", dropdownList: { list: categoriesList, status: categoriesStatus } },
-    { label: "Blog", id: "blog", path: "./blog" },
-    { label: "Contacts", id: "contacts", path: "./contacts" },
-  ];
 
   return (
-    <div className="App">
-      <Header
-        routs={routs}
-        LogoSvg={LogoSvg}
-      />
-      <main>
-        <BannerSwiper/>
-        <PopularCategories/>
-        <BannerBlock/>
-        <NewsletterBlock/>
-      </main>
-      <Footer
-        routs={routs}
-        LogoSvg={LogoSvg}/>
-    </div>
+    <main className="App">
+      <BannerSwiper/>
+      <PopularCategories/>
+      <BannerBlock/>
+      <NewsletterBlock/>
+    </main>
   );
 }
